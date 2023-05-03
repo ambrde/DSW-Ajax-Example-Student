ed449c31e7798b6a5cd07e10757176f86d8c53bd
ambrde <amanidewar@gmail.com>
2023-05-03 13:57:40 -0700

    Change divs

--- webapp.py
+++ webapp.py
@@ -8,6 +8,10 @@ app.debug = True #Change this to False for production
 @app.route('/')
 def home():
     return render_template('home.html')
+    
+@app.route('/test')
+def test():
+    return Markup('<h2>Updated Div 1</h2><p>This is the updated content from the server.</p>')
 
 if __name__ == '__main__':
     app.run()
